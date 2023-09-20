2bf5e04e759fca7e2a45deddafc96d18322a3df9
Luis Luna <101300840+LuisDeLuna-Dev@users.noreply.github.com>
2023-09-20 15:46:14 -0300

    Programa Concluido
    Programa funcionando perfeitamente.
    Falta automatizar o input de altura incluir um "." depois do primeiro digito.

--- main.js
+++ main.js
@@ -1,20 +1,18 @@
-var height = +document.getElementById("alt").value;
-var weight = +document.getElementById("pes").value;
-var imc = weight/(height*height);
-
-
 bot.addEventListener('click',function calculate(){
+    var height = +document.getElementById("alt").value;
+    var weight = +document.getElementById("pes").value;
+    var imc = weight/(height*height);
     console.log(imc)
     if(imc < 18.5){
-        document.getElementById("res").innerText="Seu IMC é de ${imc}. Você está magro. "
+        document.getElementById("res").innerText=`Seu IMC é de ${imc.toFixed(2)}. Você está magro. `
     } else if(imc<24.9) {
-        document.getElementById("res").innerText="Seu IMC é de ${imc}. Você está normal" 
+        document.getElementById("res").innerText=`Seu IMC é de ${imc.toFixed(2)}. Você está normal` 
     } else if(imc<29.9) {
-        document.getElementById("res").innerText="Seu IMC é de ${imc}. Você está sobre peso"
+        document.getElementById("res").innerText=`Seu IMC é de ${imc.toFixed(2)}. Você está sobre peso`
     }else if(imc<39.9) {
-        document.getElementById("res").innerText="Seu IMC é de ${imc}. Você está obeso" 
+        document.getElementById("res").innerText=`Seu IMC é de ${imc.toFixed(2)}. Você está obeso` 
     }else if(imc>39.9) {
-        document.getElementById("res").innerText="Seu IMC é de ${imc}. Você está com obesidade mórbida" 
+        document.getElementById("res").innerText=`Seu IMC é de ${imc.toFixed(2)}. Você está com obesidade mórbida`
     } 
     console.log(typeof weight)//confirmação de que está como um número
 })
