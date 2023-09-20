d254cfbde471d115a137216d80a3b42aac379e86
Luis Luna <101300840+LuisDeLuna-Dev@users.noreply.github.com>
2023-09-20 10:37:47 -0300

    Erro: NaN
    Corrigidos todos os erros anteriores mas agora está dando NaN após o calculo do IMC.

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
-function calculate(){
-    var height = document.getElementById(alt).value / 100;
-    var weight = document.getElementById(pes).value;
-    
-    var imc = weight(height*height)
-    var text=""
+var height = +document.getElementById("alt").value;
+var weight = +document.getElementById("pes").value;
+var imc = weight/(height*height);
+
+
+bot.addEventListener('click',function calculate(){
+    console.log(imc)
     if(imc < 18.5){
-        window.alert("Você está magro.")
+        document.getElementById("res").innerText="Seu IMC é de ${imc}. Você está magro. "
     } else if(imc<24.9) {
-        window.alert("Você está normal")
+        document.getElementById("res").innerText="Seu IMC é de ${imc}. Você está normal" 
     } else if(imc<29.9) {
-        window.alert("Você está sobre peso")
+        document.getElementById("res").innerText="Seu IMC é de ${imc}. Você está sobre peso"
     }else if(imc<39.9) {
-        window.alert("Você está obeso")
+        document.getElementById("res").innerText="Seu IMC é de ${imc}. Você está obeso" 
     }else if(imc>39.9) {
-        window.alert("Você está com obesidade mórbida")
-    }
-    document.getElementById("res").innerText="Calculou"
-    
-}
+        document.getElementById("res").innerText="Seu IMC é de ${imc}. Você está com obesidade mórbida" 
+    } 
+    console.log(typeof weight)//confirmação de que está como um número
+})
+
