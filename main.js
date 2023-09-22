0f1d7d9c1537def6b41f4ba235fc70f8312f0f8e
Luis Luna <101300840+LuisDeLuna-Dev@users.noreply.github.com>
2023-09-22 14:27:00 -0300

    Erro Corrigido e programa perfeito
    O erro anterio era do resultado dar 0.00 quando digitado uma altura sem a virgula ou ponto.
    Agora o input de altura adiciona automaticamente a "," após tirar o cursor do input.

--- main.js
+++ main.js
@@ -1,19 +1,64 @@
-bot.addEventListener('click',function calculate(){
-    var height = +document.getElementById("alt").value;
-    var weight = +document.getElementById("pes").value;
-    var imc = weight/(height*height);
-    console.log(imc)
-    if(imc < 18.5){
-        document.getElementById("res").innerText=`Seu IMC é de ${imc.toFixed(2)}. Você está magro. `
-    } else if(imc<24.9) {
-        document.getElementById("res").innerText=`Seu IMC é de ${imc.toFixed(2)}. Você está normal` 
-    } else if(imc<29.9) {
-        document.getElementById("res").innerText=`Seu IMC é de ${imc.toFixed(2)}. Você está sobre peso`
-    }else if(imc<39.9) {
-        document.getElementById("res").innerText=`Seu IMC é de ${imc.toFixed(2)}. Você está obeso` 
-    }else if(imc>39.9) {
-        document.getElementById("res").innerText=`Seu IMC é de ${imc.toFixed(2)}. Você está com obesidade mórbida`
-    } 
-    console.log(typeof weight)//confirmação de que está como um número
-})
+// Função para adicionar a máscara ao campo de entrada de altura
+function adicionarMascaraAltura() {
+    var alturaInput = document.getElementById("alt");
 
+    alturaInput.addEventListener('blur', function() {
+        var inputValue = alturaInput.value;
+
+        // Remove todos os caracteres não numéricos e pontos, exceto o primeiro ponto
+        inputValue = inputValue.replace(/[^\d.]/g, '');
+        inputValue = inputValue.replace(/^\./, '');
+
+        // Verifica se o valor não está vazio e começa com um número
+        if (inputValue.length > 0 && inputValue.indexOf('.') === -1) {
+            inputValue = inputValue.substring(0, 1) + '.' + inputValue.substring(1);
+        }
+
+        // Define o valor formatado de volta no campo de entrada
+        alturaInput.value = inputValue;
+    });
+}
+
+// Adiciona um ouvinte de evento ao botão "Calcular" para chamar a função "calculate"
+var botaoCalcular = document.getElementById("bot");
+botaoCalcular.addEventListener('click', function() {
+    calculate(); // Chama a função calculate existente
+});
+
+// Função calculate existente
+function calculate() {
+    var alturaInput = document.getElementById("alt");
+    var pesoInput = document.getElementById("pes");
+    var resultadoElemento = document.getElementById("res");
+
+    var altura = parseFloat(alturaInput.value.replace(',', '.'));
+    var peso = parseFloat(pesoInput.value.replace(',', '.'));
+
+    // Validação de entrada
+    if (isNaN(altura) || isNaN(peso) || altura <= 0 || peso <= 0) {
+        resultadoElemento.innerText = "Por favor, insira valores válidos.";
+        return;
+    }
+
+    var imc = peso / (altura * altura);
+    console.log(imc);
+
+    var mensagem = "";
+
+    if (imc < 18.5) {
+        mensagem = `Seu IMC é de ${imc.toFixed(2)}. Você está magro.`;
+    } else if (imc < 24.9) {
+        mensagem = `Seu IMC é de ${imc.toFixed(2)}. Você está normal.`;
+    } else if (imc < 29.9) {
+        mensagem = `Seu IMC é de ${imc.toFixed(2)}. Você está com sobrepeso.`;
+    } else if (imc < 39.9) {
+        mensagem = `Seu IMC é de ${imc.toFixed(2)}. Você está obeso.`;
+    } else {
+        mensagem = `Seu IMC é de ${imc.toFixed(2)}. Você está com obesidade mórbida.`;
+    }
+
+    resultadoElemento.innerText = mensagem;
+}
+
+// Adiciona um ouvinte de evento ao carregar a página para adicionar a máscara à altura
+document.addEventListener('DOMContentLoaded', adicionarMascaraAltura);
